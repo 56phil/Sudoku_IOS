47defdc01d9924c6ad630c53a79d3df1b9b0918f
Phil Huffman <philhuffman56@icloud.com>
2022-03-28 10:08:15 -0500

    Initial commit from Xcode

---
+++ Sudoku_iOS/maincpp.h
@@ -0,0 +1,18 @@
+//
+//  maincpp.hpp
+//  C++inSwiftUI
+//
+//  Created by Will Martin on 12/27/20.
+//
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+const char * getOriginal ();
+const char * solveSudoku ();
+
+#ifdef __cplusplus
+}
+#endif
